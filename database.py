55329d7d45c6e6df0ea843e1c0aedf7a11fc39dd
roliygu <roliygu@gmail.com>
2013-05-25 20:58:59 +0800

    repair the SQL'blankspaceBug

--- database.py
+++ database.py
@@ -4,15 +4,29 @@ import os
 import MySQLdb
 conn = MySQLdb.Connect(user='root',passwd='KF.moon083166139339',db='text')
 coursor=conn.cursor()
+
 def SQLinput():
 	s=raw_input('>>')
 	while s[-1] is not ';':
+		s+=' '
 		s+=raw_input('->    ')
 	return s
-s=SQLinput()
-coursor.execute(s)
-for row  in coursor.fetchall():
-    print(row)
+
+
+def show(coursor):
+	a=coursor.fetchall()
+	for row in a:
+		print(row)
+
+
+def SQL():
+	coursor=conn.cursor()
+	s=SQLinput()
+	if coursor.execute(s) != 0:
+		show(coursor)
+
+SQL()
+x=input("a")
 coursor.close()
 conn.close()
-input()
+
